a64fb3e67fda51590d2edfddadf75168409f7334
pythonprogramming <harrison@sentdex.com>
2015-03-25 19:40:14 -0700

    init commit

---
+++ 1. simple print function and string rules.py
@@ -0,0 +1,23 @@
+print('This is an example of the print function')
+
+# while we are here, let's talk a bit about strings #
+
+print("double quotes")
+
+print('or single...')
+
+print('concatena'+'tion')
+
+print('can do this',5)
+#print('cannot do this:'+5)
+
+#print('Can't do this')
+print('you\'ll have success here')
+print("you'll have sucess here too")
+      
+
+
+'''
+These are just the basics of strings for now, you can also pass
+variables and do all sorts of neat things. 
+'''
